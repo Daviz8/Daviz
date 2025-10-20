c5736a4dba24203b255cccd2fade9f0df158cfb0
mac <mac@Davizs-MacBook-Pro.local>
2025-10-20 07:56:31 +0100

    email form fix

--- assets/js/main.js
+++ assets/js/main.js
@@ -157,22 +157,3 @@
 		});
 
 })(jQuery);
-
-function sendEmail() {
-	const templateParams = {
-		name: document.querySelector("#name").value,
-		email:document.querySelector('#email').value,
-		subject:document.querySelector("#subject").value,
-		message:document.querySelector("#message").value,
-	};
-
-emailjs.send('service_65lhgli', 'template_hlkly9r', templateParams).then(
-  (response) => {
-    console.log('SUCCESS!', response.status, response.text);
-  },
-  (error) => {
-    console.log('FAILED...', error);
-  },
-);
-
-};
